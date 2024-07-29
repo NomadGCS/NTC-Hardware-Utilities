3b9973514026399a6d06434a413e2d9d3c6d6081
johnbissennomad <143540539+johnbissennomad@users.noreply.github.com>
2024-07-29 12:03:12 -0600

    Fix issue with copy to clipboard

--- src/webapps/configBuilder/src/util/configBuilderPageFunctions.js
+++ src/webapps/configBuilder/src/util/configBuilderPageFunctions.js
@@ -1,28 +1,13 @@
 
-export function copyToClipBoard() {
-    // const copyText = document.getElementById("jsonResult");
-    //
-    // // Select the text field
-    // copyText.select();
-    // copyText.setSelectionRange(0, 99999); // For mobile devices
-
+export async function copyToClipBoard() {
     let text = copyTextFromInput("jsonResult")
 
     // Copy the text inside the text field
-    navigator.clipboard.writeText(text);
-
+    await parent.navigator.clipboard.writeText(text);
     console.log('text: ', text)
 
     // Alert the copied text
-    alert(text);
-
-    // TODO:  There may be some bugs around closing the alert (...document not focused...).  Need to test without dev tools open for sure.
-
-    // ------------------------------
-
-    // clipboard.writeText(clippy_button.href).then(function(x) {
-    //     alert("Link copied to clipboard: " + clippy_button.href);
-    //   });
+    alert('JSON copied to clipboard');    
 
 }
 
